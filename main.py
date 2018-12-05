2f7f584352ae1582b840754af060625763ec2112
jack.s <my_email@exp.co>
2018-12-05 19:46:23 +0800

    add beautifulsoup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import time 
 from lxml import etree
 
+from bs4 import BeautifulSoup
 url='https://book.douban.com/top250?start=0'
 def get_one_page(url):
     headers={
@@ -12,7 +13,7 @@ def get_one_page(url):
     html=response.text
     return html
 
-def parse_page(html):
+def parse_page_xpath(html):
     html=etree.HTML(html)
     results=html.xpath('//body//div[@id="content"]//div[@class="indent"]/table')
 #    for result in results:
@@ -29,9 +30,26 @@ def parse_page(html):
         print(results[i].xpath('//p[@class="pl"]/text()'))
 
 
+
+def parse_page_beautifulsoup_base(html):
+    soup=BeautifulSoup(html,'lxml')
+    result=soup.html.body.find(name='div',class_='indent')
+    firststep=result.find_all(name="table")
+    book={}
+    for item in firststep:
+        sedstep=item.tr.find(name="div",attrs={"class":"pl2"})
+        book["title"]=sedstep.a.get_text()
+        book["author"]=item.find(name="p",attrs={'class':'pl'}).string
+        book["rating"]=item.find(class_="rating_nums").string
+        book["notes"]=item.find(class_="inq").string 
+        print(book)
+
+
+    
+
 def main():
     html=get_one_page(url)
-    parse_page(html)
+    parse_page_beautifulsoup_base(html)
 
 
 
